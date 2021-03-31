3b26df252d37149028c228a4579b8385d6fed30d
Phil Buchanan <info@philbuchanan.com>
2021-03-31 16:54:11 -0400

    Fixed width of heading level picker popover (#124)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill'), 'version' => 'e7a47a5ceadf9629548ba616a0fee392');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill'), 'version' => '84e71aa3659a6d0dbc365ce66ce02441');
